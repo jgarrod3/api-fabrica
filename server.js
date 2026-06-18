eaf833369c1087beb287a91257935456fe071a34
jgarrod3 <jgarrod3@epsa.upv.es>
2026-06-18 11:33:34 +0200

    Añadida interfaz grafica y nuevo server de bnuevo

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Panel de control web
+app.use(express.static(path.join(__dirname, 'public'))); // Panel de control web
 
 // ─────────────────────────────────────────────
 // Estado inicial de la fábrica
